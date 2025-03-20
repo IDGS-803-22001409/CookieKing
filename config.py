780596575dd95d79b1d2d3fc7cc2be2deaec77b4
IDGS-803-22001409 <81347@alumnos.utleon.edu.mx>
2025-03-19 18:12:23 -0600

    Creacion del proyecto

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from sqlalchemy import create_engine
+
+import urllib
+
+class Config:
+    SECRET_KEY = "1234567890"
+    SESSION_COOKIE_SECURE = False
+
+class DevelopmentConfig(Config):  
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:sergio@localhost:3306/flaskBD'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
